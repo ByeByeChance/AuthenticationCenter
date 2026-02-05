fc6974719a71de66d15a1907d3c66cd5e9d2b4c2
Chance <17621431@qq.com>
2026-02-05 09:43:57 +0800

    feat: 增加ReactBits组件

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,13 +1,15 @@
 import "./index.less";
 import HomeCard from "./components/card";
 import SplitText from "@/components/ReactBits/SplitText";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const username = useSelector((state: any) => state.user.userInfo.name);
   return (
     <div className="home-wrap">
       <div className="home-content-title">
         <SplitText
-          text="请选择一个系统进行登入！"
+          text={`${username}，请选择一个系统进行登入！`}
           delay={50}
           duration={1.25}
           ease="power3.out"
